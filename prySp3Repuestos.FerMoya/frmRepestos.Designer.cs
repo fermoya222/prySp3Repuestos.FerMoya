1bd3bc065d62f3645ea17c3712c6faad23a3780a
Alumnos <Alumnos@PC61>
2022-11-02 12:12:08 -0300

    Agregar archivos de proyecto.

---
+++ prySp3Repuestos.FerMoya/frmRepestos.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace prySp3Repuestos.FerMoya
+{
+    partial class FrmRepuestos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRepuestos));
+            this.mrcDatos = new System.Windows.Forms.GroupBox();
+            this.rtxtDescripcion = new System.Windows.Forms.RichTextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtOrigen = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.lblOrigen = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.mrcIngresados = new System.Windows.Forms.GroupBox();
+            this.rtxtDescripcion2 = new System.Windows.Forms.RichTextBox();
+            this.lblDescripcion2 = new System.Windows.Forms.Label();
+            this.txtPrecio2 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.lblPrecio2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.mrcOrigen = new System.Windows.Forms.GroupBox();
+            this.optImportado = new System.Windows.Forms.RadioButton();
+            this.optNacional = new System.Windows.Forms.RadioButton();
+            this.lblMarca2 = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.mrcDatos.SuspendLayout();
+            this.mrcIngresados.SuspendLayout();
+            this.mrcOrigen.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mrcDatos
+            // 
+            this.mrcDatos.Controls.Add(this.rtxtDescripcion);
+            this.mrcDatos.Controls.Add(this.lblDescripcion);
+            this.mrcDatos.Controls.Add(this.txtPrecio);
+            this.mrcDatos.Controls.Add(this.txtNumero);
+            this.mrcDatos.Controls.Add(this.lblPrecio);
+            this.mrcDatos.Controls.Add(this.lblNumero);
+            this.mrcDatos.Controls.Add(this.txtOrigen);
+            this.mrcDatos.Controls.Add(this.txtMarca);
+            this.mrcDatos.Controls.Add(this.lblOrigen);
+            this.mrcDatos.Controls.Add(this.lblMarca);
+            this.mrcDatos.Font = new System.Drawing.Font("Myanmar Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mrcDatos.Location = new System.Drawing.Point(23, 22);
+            this.mrcDatos.Name = "mrcDatos";
+            this.mrcDatos.Size = new System.Drawing.Size(393, 167);
+            this.mrcDatos.TabIndex = 0;
+            this.mrcDatos.TabStop = false;
+            this.mrcDatos.Text = "Datos del Repuesto";
+            // 
+            // rtxtDescripcion
+            // 
+            this.rtxtDescripcion.Location = new System.Drawing.Point(126, 106);
+            this.rtxtDescripcion.Name = "rtxtDescripcion";
+            this.rtxtDescripcion.Size = new System.Drawing.Size(176, 42);
+            this.rtxtDescripcion.TabIndex = 9;
+            this.rtxtDescripcion.Text = "";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(181, 83);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(69, 20);
+            this.lblDescripcion.TabIndex = 8;
+            this.lblDescripcion.Text = "Descripcion";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(289, 46);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(84, 28);
+            this.txtPrecio.TabIndex = 7;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(289, 24);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(84, 28);
+            this.txtNumero.TabIndex = 6;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(238, 49);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(40, 20);
+            this.lblPrecio.TabIndex = 5;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(238, 27);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(50, 20);
+            this.lblNumero.TabIndex = 4;
+            this.lblNumero.Text = "Numero";
+            // 
+            // txtOrigen
+            // 
+            this.txtOrigen.Location = new System.Drawing.Point(78, 46);
+            this.txtOrigen.Name = "txtOrigen";
+            this.txtOrigen.Size = new System.Drawing.Size(85, 28);
+            this.txtOrigen.TabIndex = 3;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(78, 23);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(85, 28);
+            this.txtMarca.TabIndex = 2;
+            // 
+            // lblOrigen
+            // 
+            this.lblOrigen.AutoSize = true;
+            this.lblOrigen.Location = new System.Drawing.Point(23, 49);
+            this.lblOrigen.Name = "lblOrigen";
+            this.lblOrigen.Size = new System.Drawing.Size(45, 20);
+            this.lblOrigen.TabIndex = 1;
+            this.lblOrigen.Text = "Origen";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(24, 27);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(40, 20);
+            this.lblMarca.TabIndex = 0;
+            this.lblMarca.Text = "Marca";
+            // 
+            // mrcIngresados
+            // 
+            this.mrcIngresados.Controls.Add(this.rtxtDescripcion2);
+            this.mrcIngresados.Controls.Add(this.lblDescripcion2);
+            this.mrcIngresados.Controls.Add(this.txtPrecio2);
+            this.mrcIngresados.Controls.Add(this.txtNumero2);
+            this.mrcIngresados.Controls.Add(this.lblPrecio2);
+            this.mrcIngresados.Controls.Add(this.comboBox1);
+            this.mrcIngresados.Controls.Add(this.lblNumero2);
+            this.mrcIngresados.Controls.Add(this.mrcOrigen);
+            this.mrcIngresados.Controls.Add(this.lblMarca2);
+            this.mrcIngresados.Font = new System.Drawing.Font("Myanmar Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mrcIngresados.Location = new System.Drawing.Point(25, 195);
+            this.mrcIngresados.Name = "mrcIngresados";
+            this.mrcIngresados.Size = new System.Drawing.Size(391, 234);
+            this.mrcIngresados.TabIndex = 1;
+            this.mrcIngresados.TabStop = false;
+            this.mrcIngresados.Text = "Repuestos Ingresados";
+            // 
+            // rtxtDescripcion2
+            // 
+            this.rtxtDescripcion2.Location = new System.Drawing.Point(124, 174);
+            this.rtxtDescripcion2.Name = "rtxtDescripcion2";
+            this.rtxtDescripcion2.Size = new System.Drawing.Size(176, 42);
+            this.rtxtDescripcion2.TabIndex = 10;
+            this.rtxtDescripcion2.Text = "";
+            // 
+            // lblDescripcion2
+            // 
+            this.lblDescripcion2.AutoSize = true;
+            this.lblDescripcion2.Location = new System.Drawing.Point(179, 151);
+            this.lblDescripcion2.Name = "lblDescripcion2";
+            this.lblDescripcion2.Size = new System.Drawing.Size(69, 20);
+            this.lblDescripcion2.TabIndex = 10;
+            this.lblDescripcion2.Text = "Descripcion";
+            // 
+            // txtPrecio2
+            // 
+            this.txtPrecio2.Location = new System.Drawing.Point(287, 65);
+            this.txtPrecio2.Name = "txtPrecio2";
+            this.txtPrecio2.Size = new System.Drawing.Size(84, 28);
+            this.txtPrecio2.TabIndex = 10;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(287, 43);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(84, 28);
+            this.txtNumero2.TabIndex = 10;
+            // 
+            // lblPrecio2
+            // 
+            this.lblPrecio2.AutoSize = true;
+            this.lblPrecio2.Location = new System.Drawing.Point(243, 73);
+            this.lblPrecio2.Name = "lblPrecio2";
+            this.lblPrecio2.Size = new System.Drawing.Size(40, 20);
+            this.lblPrecio2.TabIndex = 10;
+            this.lblPrecio2.Text = "Precio";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(76, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(85, 28);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Location = new System.Drawing.Point(239, 50);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(50, 20);
+            this.lblNumero2.TabIndex = 3;
+            this.lblNumero2.Text = "Numero";
+            // 
+            // mrcOrigen
+            // 
+            this.mrcOrigen.Controls.Add(this.optImportado);
+            this.mrcOrigen.Controls.Add(this.optNacional);
+            this.mrcOrigen.Location = new System.Drawing.Point(26, 73);
+            this.mrcOrigen.Name = "mrcOrigen";
+            this.mrcOrigen.Size = new System.Drawing.Size(125, 70);
+            this.mrcOrigen.TabIndex = 1;
+            this.mrcOrigen.TabStop = false;
+            this.mrcOrigen.Text = "Origen";
+            // 
+            // optImportado
+            // 
+            this.optImportado.AutoSize = true;
+            this.optImportado.Location = new System.Drawing.Point(6, 39);
+            this.optImportado.Name = "optImportado";
+            this.optImportado.Size = new System.Drawing.Size(81, 24);
+            this.optImportado.TabIndex = 1;
+            this.optImportado.TabStop = true;
+            this.optImportado.Text = "Importado";
+            this.optImportado.UseVisualStyleBackColor = true;
+            // 
+            // optNacional
+            // 
+            this.optNacional.AutoSize = true;
+            this.optNacional.Location = new System.Drawing.Point(7, 19);
+            this.optNacional.Name = "optNacional";
+            this.optNacional.Size = new System.Drawing.Size(72, 24);
+            this.optNacional.TabIndex = 0;
+            this.optNacional.TabStop = true;
+            this.optNacional.Text = "Nacional";
+            this.optNacional.UseVisualStyleBackColor = true;
+            // 
+            // lblMarca2
+            // 
+            this.lblMarca2.AutoSize = true;
+            this.lblMarca2.Location = new System.Drawing.Point(21, 32);
+            this.lblMarca2.Name = "lblMarca2";
+            this.lblMarca2.Size = new System.Drawing.Size(40, 20);
+            this.lblMarca2.TabIndex = 0;
+            this.lblMarca2.Text = "Marca";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Myanmar Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(442, 73);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 2;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Myanmar Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(442, 166);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Font = new System.Drawing.Font("Myanmar Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.Location = new System.Drawing.Point(442, 405);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(70, 24);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // FrmRepuestos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(549, 457);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.mrcIngresados);
+            this.Controls.Add(this.mrcDatos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmRepuestos";
+            this.Text = "Repuestos";
+            this.mrcDatos.ResumeLayout(false);
+            this.mrcDatos.PerformLayout();
+            this.mrcIngresados.ResumeLayout(false);
+            this.mrcIngresados.PerformLayout();
+            this.mrcOrigen.ResumeLayout(false);
+            this.mrcOrigen.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mrcDatos;
+        private System.Windows.Forms.RichTextBox rtxtDescripcion;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox txtOrigen;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.Label lblOrigen;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.GroupBox mrcIngresados;
+        private System.Windows.Forms.Label lblMarca2;
+        private System.Windows.Forms.RichTextBox rtxtDescripcion2;
+        private System.Windows.Forms.Label lblDescripcion2;
+        private System.Windows.Forms.TextBox txtPrecio2;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Label lblPrecio2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.GroupBox mrcOrigen;
+        private System.Windows.Forms.RadioButton optImportado;
+        private System.Windows.Forms.RadioButton optNacional;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnConsultar;
+    }
+}
+
